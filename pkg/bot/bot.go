8b03f129b0eb1c45465332267e03aa17a362e8e7
Jose Eduardo de C. Silva <jose.carvalho@caed.ufjf.br>
2024-08-05 22:40:14 -0300

    Adding missing files

---
+++ pkg/bot/bot.go
@@ -0,0 +1,210 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	badger "github.com/dgraph-io/badger/v3"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Config struct {
+	BrokerURL         string         `yaml:"broker_url"`
+	BrokerUsername    string         `yaml:"broker_username"`
+	BrokerPassword    string         `yaml:"broker_password"`
+	FrigateURL        string         `yaml:"frigate_url"`
+	TelegramToken     string         `yaml:"telegram_token"`
+	TelegramChatID    int64          `yaml:"telegram_chat_id"`
+	DataFolder        string         `yaml:"data_folder"`
+	RequiredZoneCount map[string]int `yaml:"camera_required_zone_count"`
+}
+
+type bot struct {
+	tb        *tgbotapi.BotAPI
+	client    mqtt.Client
+	events    map[string]event
+	lastEvent string
+	cfg       Config
+	db        *badger.DB
+}
+
+func Run(cfg Config) error {
+
+	b := &bot{
+		cfg:    cfg,
+		events: map[string]event{},
+	}
+
+	db, err := badger.Open(badger.DefaultOptions(cfg.DataFolder))
+	if err != nil {
+		log.Fatalf("[Open] error: %v", err)
+		return err
+	}
+	defer db.Close()
+	b.db = db
+
+	//connect to telegram
+	tb, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
+	if err != nil {
+		log.Fatalf("[NewBotAPI] error: %v", err)
+		return err
+	}
+	log.Printf("Telegram authorized on account %s", tb.Self.UserName)
+	b.tb = tb
+
+	go b.handleTelegram()
+
+	//connect to mqtt
+	opts := mqttOpts(cfg)
+	opts.SetDefaultPublishHandler(b.handlePublishedMsgs)
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	if token := client.Subscribe("frigate/#", 1, nil); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	b.client = client
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	return nil
+}
+
+func mqttOpts(cfg Config) *mqtt.ClientOptions {
+//	var broker = "192.168.0.26"
+//    var port = 1883
+//    opts := mqtt.NewClientOptions()
+//    opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+//    opts.SetClientID("go_mqtt_client")
+
+	opts := mqtt.NewClientOptions()
+	opts.SetKeepAlive(60 * time.Second)
+	opts.AddBroker(cfg.BrokerURL)
+	opts.SetClientID("notifybot-client")
+	if cfg.BrokerUsername != "" {
+		opts.SetUsername(cfg.BrokerUsername)
+		opts.SetPassword(cfg.BrokerPassword)
+	}
+	return opts
+}
+
+func (b *bot) handleTelegram() {
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := b.tb.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			msg := update.Message
+			log.Printf(
+				"Message [%s: %v in %v]: %s\n",
+				update.Message.From.UserName,
+				update.Message.From.ID,
+				update.Message.Chat.ID,
+				update.Message.Text,
+			)
+			//do not respond if not in the group
+			if msg.Chat.ID != b.cfg.TelegramChatID {
+				log.Printf("Wrong chat id %v, expecting %v", msg.Chat.ID, b.cfg.TelegramChatID)
+				continue
+			}
+			if !msg.IsCommand() {
+				continue
+			}
+
+			resp := tgbotapi.NewMessage(b.cfg.TelegramChatID, "")
+			resp.ReplyToMessageID = msg.MessageID
+			//check if it's a reply
+			if msg.ReplyToMessage != nil {
+				//grab event id from db
+				var key string
+				err := b.db.View(func(txn *badger.Txn) error {
+					item, err := txn.Get([]byte(strconv.Itoa(msg.ReplyToMessage.MessageID)))
+					if err != nil {
+						return err
+					}
+					item.Value(func(val []byte) error {
+						key = string(val)
+						return nil
+					})
+					return nil
+				})
+				if err != nil {
+					log.Printf("Error grabbing key from response id: %v", err)
+					resp.Text = "Sorry I couldn't find this event"
+					b.tb.Send(resp)
+				} else {
+					log.Printf("Key %v detected\n", key)
+					switch msg.Command() {
+					case "snapshot":
+						b.sendSnapshot(key, msg.MessageID)
+					case "clip":
+						b.sendClip(key, msg.MessageID)
+					}
+				}
+				continue
+			}
+
+			//otherwise last id
+			log.Printf("Not a response: %v", msg.ReplyToMessage)
+
+			evt, ok := b.events[b.lastEvent]
+			if !ok {
+				resp.Text = "No events yet!"
+				b.tb.Send(resp)
+				continue
+			}
+
+			switch msg.Command() {
+			case "snapshot":
+				b.sendSnapshot(evt.After.ID, msg.MessageID)
+			case "clip":
+				if !evt.ended {
+					next := tgbotapi.NewMessage(
+						b.cfg.TelegramChatID,
+						fmt.Sprintf("Event id %v not ended yet; no clip available", b.lastEvent),
+					)
+					next.ReplyToMessageID = msg.MessageID
+					b.tb.Send(next)
+					break
+				}
+				b.sendClip(evt.After.ID, msg.MessageID)
+			}
+		}
+	}
+}
+
+func (b *bot) handlePublishedMsgs(client mqtt.Client, msg mqtt.Message) {
+	topics := strings.Split(msg.Topic(), "/")
+	if len(topics) < 2 {
+		return
+	}
+	if topics[0] != "frigate" {
+		return
+	}
+
+	switch topics[1] {
+	case "stats":
+	case "events":
+		b.handleEvents(topics, msg.Payload())
+	default:
+		// fmt.Printf("Received from topic %v message; skipped\n", msg.Topic())
+	}
+
+}
---
+++ pkg/bot/events.go
@@ -0,0 +1,214 @@
+package bot
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	badger "github.com/dgraph-io/badger/v3"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type event struct {
+	Type   string      `json:"type"`
+	Before eventDetail `json:"before"`
+	After  eventDetail `json:"after"`
+	//custom stuff
+	pic   []byte
+	ended bool
+}
+
+func (b *bot) media(id, media string) ([]byte, error) {
+	///api/events/<id>/thumbnail.jpg
+	response, err := http.Get(fmt.Sprintf("%v/api/events/%v/%v", b.cfg.FrigateURL, id, media))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, errors.New("received non 200 response code")
+	}
+
+	return io.ReadAll(response.Body)
+}
+
+type eventDetail struct {
+	ID              string      `json:"id"`
+	Camera          string      `json:"camera"`
+	FrameTime       float64     `json:"frame_time"`
+	SnapshotTime    float64     `json:"snapshot_time"`
+	Label           string      `json:"label"`
+	TopScore        float64     `json:"top_score"`
+	FalsePositive   bool        `json:"false_positive"`
+	StartTime       float64     `json:"start_time"`
+	EndTime         float64     `json:"end_time"`
+	Score           float64     `json:"score"`
+	Box             []int       `json:"box"`
+	Area            int         `json:"area"`
+	Region          []int       `json:"region"`
+	CurrentZones    []string    `json:"current_zones"`
+	EnteredZones    []string    `json:"entered_zones"`
+	Thumbnail       interface{} `json:"thumbnail"`
+	HasSnapshot     bool        `json:"has_snapshot"`
+	HasClip         bool        `json:"has_clip"`
+	Stationary      bool        `json:"stationary"`
+	MotionlessCount int         `json:"motionless_count"`
+	PositionChanges int         `json:"position_changes"`
+}
+
+func (b *bot) handleEvents(topics []string, data []byte) {
+	var evt event
+	err := json.Unmarshal(data, &evt)
+	if err != nil {
+		log.Printf("error reading event: %v", err)
+		return
+	}
+
+	//ignore if stationary
+	if evt.After.Stationary {
+		fmt.Printf("Stationary object %v detect on %v\n", evt.After.Label, evt.After.Camera)
+		return
+	}
+
+	//ignore if zone count not met
+	if count, ok := b.cfg.RequiredZoneCount[evt.After.Camera]; ok {
+		if len(evt.After.EnteredZones) < count {
+			fmt.Printf("Object %v detected on %v but only entered zones %v\n", evt.After.Label, evt.After.Camera, evt.After.EnteredZones)
+			return
+		}
+	}
+
+	fmt.Printf("%v detect on %v\n", evt.After.Label, evt.After.Camera)
+
+	b.lastEvent = evt.After.ID
+	_, ok := b.events[evt.After.ID]
+	//if first time grab a picture and send it to the chat
+	if !ok {
+		if snapshot, err := b.media(evt.After.ID, "snapshot.jpg"); err != nil {
+			log.Printf("Error getting snapshot for id %v: %v\n", evt.After.ID, err)
+			b.tb.Send(tgbotapi.NewMessage(
+				b.cfg.TelegramChatID,
+				fmt.Sprintf("New %v detected on camera %v (id: %v). Sorry I couldn't get a snapshot :(", evt.After.Label, evt.After.Camera, evt.After.ID),
+			))
+			evt.pic = snapshot
+		} else {
+			// msg, err := b.tb.Send(tgbotapi.NewMessage(
+			// 	b.cfg.TelegramChatID,
+			// 	fmt.Sprintf("New %v detected on camera %v (id: %v).", evt.After.Label, evt.After.Camera, evt.After.ID),
+			// ))
+			// if err == nil {
+			// 	err = b.db.Update(func(txn *badger.Txn) error {
+			// 		e := badger.NewEntry(
+			// 			[]byte(strconv.Itoa(msg.MessageID)),
+			// 			[]byte(evt.After.ID),
+			// 		).WithTTL(time.Hour * 24 * 10)
+			// 		err := txn.SetEntry(e)
+			// 		return err
+			// 	})
+			// 	if err != nil {
+			// 		log.Printf("Error saving message id into db: %v", err)
+			// 	}
+			// }
+
+			photoFileBytes := tgbotapi.FileBytes{
+				Name:  "snapshot",
+				Bytes: snapshot,
+			}
+			photo := tgbotapi.NewPhoto(b.cfg.TelegramChatID, photoFileBytes)
+			msg, err := b.tb.Send(photo)
+			if err == nil {
+				err = b.db.Update(func(txn *badger.Txn) error {
+					e := badger.NewEntry(
+						[]byte(strconv.Itoa(msg.MessageID)),
+						[]byte(evt.After.ID),
+					).WithTTL(time.Hour * 24 * 10)
+					err := txn.SetEntry(e)
+					return err
+				})
+				if err != nil {
+					log.Printf("Error saving message id into db: %v", err)
+				}
+			}
+		}
+	}
+	switch evt.Type {
+	case "update":
+		if ok {
+			//update our thumbnail
+			if snapshot, err := b.media(evt.After.ID, "snapshot.jpg"); err == nil {
+				evt.pic = snapshot
+			}
+		}
+	case "end":
+		// b.tb.Send(tgbotapi.NewMessage(
+		// 	b.cfg.TelegramChatID,
+		// 	fmt.Sprintf("Event id %v ended; clip now available", evt.After.ID),
+		// ))
+		evt.ended = true
+	}
+	b.events[evt.After.ID] = evt
+}
+
+func (b *bot) sendSnapshot(id string, replyTo int) {
+	pic, err := b.media(id, "snapshot.jpg")
+	if err != nil {
+		msg := tgbotapi.NewMessage(b.cfg.TelegramChatID, fmt.Sprintf("Sorry! Error occured grabbing snapshot for id %v", b.lastEvent))
+		msg.ReplyToMessageID = replyTo
+		b.tb.Send(msg)
+		return
+	}
+	photoFileBytes := tgbotapi.FileBytes{
+		Name:  "snapshot",
+		Bytes: pic,
+	}
+	photo := tgbotapi.NewPhoto(b.cfg.TelegramChatID, photoFileBytes)
+	msg, err := b.tb.Send(photo)
+	if err == nil {
+		err = b.db.Update(func(txn *badger.Txn) error {
+			e := badger.NewEntry(
+				[]byte(strconv.Itoa(msg.MessageID)),
+				[]byte(id),
+			).WithTTL(time.Hour * 24 * 10)
+			err := txn.SetEntry(e)
+			return err
+		})
+		if err != nil {
+			log.Printf("Error saving message id into db: %v", err)
+		}
+	}
+}
+
+func (b *bot) sendClip(id string, replyTo int) {
+	vid, err := b.media(id, "clip.mp4")
+	if err != nil {
+		msg := tgbotapi.NewMessage(b.cfg.TelegramChatID, fmt.Sprintf("Sorry! Error occured grabbing snapshot for id %v", b.lastEvent))
+		msg.ReplyToMessageID = replyTo
+		b.tb.Send(msg)
+		return
+	}
+	vidBytes := tgbotapi.FileBytes{
+		Name:  "clip",
+		Bytes: vid,
+	}
+	video := tgbotapi.NewVideo(b.cfg.TelegramChatID, vidBytes)
+	msg, err := b.tb.Send(video)
+	if err == nil {
+		err = b.db.Update(func(txn *badger.Txn) error {
+			e := badger.NewEntry(
+				[]byte(strconv.Itoa(msg.MessageID)),
+				[]byte(id),
+			).WithTTL(time.Hour * 24 * 10)
+			err := txn.SetEntry(e)
+			return err
+		})
+		if err != nil {
+			log.Printf("Error saving message id into db: %v", err)
+		}
+	}
+}
